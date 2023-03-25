f82b2c29351cb9d2969570c6292e42e55a9956c5
ChuSeongJun <91831079+ChuSeongJun@users.noreply.github.com>
2023-03-25 20:35:07 +0900

    Feat: recoil, react-query 적용

--- client.tsx
+++ client.tsx
@@ -4,6 +4,8 @@ import React from "react";
 import { render } from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import axios from "axios";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { RecoilRoot } from "recoil";
 
 import App from "@layouts/App/App";
 
@@ -13,9 +15,15 @@ axios.defaults.baseURL =
     ? "사이트 주소"
     : "http://localhost:3090";
 
+const queryClient = new QueryClient();
+
 render(
   <BrowserRouter>
-    <App />
+    <RecoilRoot>
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </RecoilRoot>
   </BrowserRouter>,
   document.querySelector("#app")
 );
