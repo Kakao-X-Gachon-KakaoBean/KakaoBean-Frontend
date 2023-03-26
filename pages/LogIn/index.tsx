f18330277fe58ad76aefb94419dfdfedbf57161f
ChuSeongJun <91831079+ChuSeongJun@users.noreply.github.com>
2023-03-26 22:54:52 +0900

    Feat: 구글 로그인 패키지 설치

--- pages/LogIn/index.tsx
+++ pages/LogIn/index.tsx
@@ -1,7 +1,21 @@
 import React from "react";
 
+import { GoogleLogin } from "@react-oauth/google";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+
 const LogIn = () => {
-  return <div>로그인 페이지</div>;
+  const clientId = "";
+  return (
+    <>
+      <GoogleOAuthProvider clientId={clientId}>
+        <GoogleLogin
+          onSuccess={(res) => {
+            console.log(res);
+          }}
+        />
+      </GoogleOAuthProvider>
+    </>
+  );
 };
 
 export default LogIn;
