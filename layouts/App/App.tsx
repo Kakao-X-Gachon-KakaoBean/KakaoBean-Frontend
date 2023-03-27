399b083f01a6635494062f71502b4dfe7cd7c82a
ChuSeongJun <91831079+ChuSeongJun@users.noreply.github.com>
2023-03-27 10:05:54 +0900

    Feat: 파일 경로 설정

--- layouts/App/App.tsx
+++ layouts/App/App.tsx
@@ -5,6 +5,9 @@ import loadable from "@loadable/component";
 const Main = loadable(() => import(`@layouts/Main`));
 const SignUp = loadable(() => import(`@pages/SignUp`));
 const LogIn = loadable(() => import(`@pages/LogIn`));
+const MyPage = loadable(() => import(`@pages/MyPage`));
+const Team = loadable(() => import(`@pages/Team`));
+const Product = loadable(() => import(`@pages/Product`));
 
 const App = () => {
   return (
@@ -15,6 +18,9 @@ const App = () => {
       <Route path="/main" component={Main} />
       <Route path="/signup" component={SignUp} />
       <Route path="/login" component={LogIn} />
+      <Route path="/team" component={Team} />
+      <Route path="/product" component={Product} />
+      <Route path="/mypage" component={MyPage} />
     </Switch>
   );
 };
