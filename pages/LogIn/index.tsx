d989e05d02706ab1b48637b9c0b6ab6327892434
ChuSeongJun <91831079+ChuSeongJun@users.noreply.github.com>
2023-03-29 23:29:52 +0900

    Chore: 코드 구조 변경

--- pages/LogIn/index.tsx
+++ pages/LogIn/index.tsx
@@ -1,19 +1,77 @@
-import React from "react";
+import React, { FormEvent, useCallback } from "react";
 
 import { GoogleLogin } from "@react-oauth/google";
 import { GoogleOAuthProvider } from "@react-oauth/google";
+import useInput from "@hooks/useInput";
+import {
+  Form,
+  Header,
+  Input,
+  Label,
+  LoginBtn,
+  SearchBox,
+  Wrapper,
+} from "@pages/LogIn/styles";
+import { Link } from "react-router-dom";
 
 const LogIn = () => {
   const clientId = "";
+
+  const [email, onChangeEmail, setEmail] = useInput("");
+  const [password, onChangePassword, setPassword] = useInput("");
+
+  const onSubmit = useCallback(
+    (e: FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+    },
+    [email, password]
+  );
+
   return (
     <>
-      <GoogleOAuthProvider clientId={clientId}>
-        <GoogleLogin
-          onSuccess={(res) => {
-            console.log(res);
-          }}
-        />
-      </GoogleOAuthProvider>
+      <Wrapper>
+        <Header>BeanBay</Header>
+        <Form onSubmit={onSubmit}>
+          <Label>
+            <Input
+              type="text"
+              id="id"
+              name="id"
+              value={email}
+              onChange={onChangeEmail}
+              placeholder="이메일"
+            />
+          </Label>
+          <Label>
+            <Input
+              type="password"
+              id="password"
+              name="password"
+              value={password}
+              onChange={onChangePassword}
+              placeholder="비밀번호"
+            />
+          </Label>
+          <LoginBtn type="submit">로그인</LoginBtn>
+          <SearchBox>
+            <span>비밀번호 찾기</span>
+            <span>
+              <span>신규 회원이신가요? </span>
+              <Link style={{ fontWeight: "bold" }} to="/signup">
+                회원 가입
+              </Link>
+            </span>
+          </SearchBox>
+        </Form>
+        <div>또는</div>
+        <GoogleOAuthProvider clientId={clientId}>
+          <GoogleLogin
+            onSuccess={(res) => {
+              console.log(res);
+            }}
+          />
+        </GoogleOAuthProvider>
+      </Wrapper>
     </>
   );
 };
---
+++ pages/LogIn/styles.tsx
@@ -0,0 +1,100 @@
+import styled from "@emotion/styled";
+
+export const Wrapper = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: #ffffff;
+`;
+export const Header = styled.div`
+  font-size: 4em;
+  font-weight: 300;
+  margin-bottom: 0.8rem;
+  line-height: 77px;
+  letter-spacing: 0.1em;
+  text-align: center;
+`;
+export const Form = styled.form`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+
+export const Label = styled.div`
+  margin-bottom: 0.8rem;
+
+  & span {
+    text-align: left;
+    font-size: 15px;
+    cursor: pointer;
+    font-weight: 700;
+    width: 7rem;
+  }
+`;
+export const Input = styled.input`
+  color: #969696;
+  background-color: #ffffff;
+  position: relative;
+  box-sizing: border-box;
+  box-shadow: 0 4px 4px 0 #00000040;
+  border: 1px solid #969696;
+  border-radius: 12px;
+  width: 32rem;
+  height: 4rem;
+  outline: none;
+  font-size: 1.3rem;
+  padding: 10px 15px 12px;
+  margin-top: 0.6rem;
+  :focus {
+    border: 1px solid gray;
+  }
+  ::placeholder {
+    font-size: 1.3rem;
+    font-weight: 200;
+    color: #969696;
+  }
+`;
+
+export const SearchBox = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 32rem;
+  font-size: 1rem;
+  margin-top: 0.8rem;
+  & span {
+    padding: 0.5rem;
+    color: #343434;
+    & a {
+      text-decoration: none;
+      color: inherit;
+    }
+  }
+`;
+
+export const LoginBtn = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  vertical-align: middle;
+  text-align: center;
+  width: 32rem;
+  height: 4rem;
+  background-color: #8cafa3;
+  box-shadow: 0 4px 4px 0 #00000040;
+  color: white;
+  border-radius: 10px;
+  outline: none;
+  font-size: 1.5rem;
+  font-weight: 700;
+  cursor: pointer;
+  margin-top: 1rem;
+  border: none;
+  &:hover {
+    background-color: #969696;
+    border: none;
+  }
+`;
