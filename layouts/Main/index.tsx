27a4ede747bee2278249f80e837a3f9ea5cb4ba3
jomo34 <trillion32@naver.com>
2023-04-02 21:03:51 +0900

    main page inital

--- layouts/Main/index.tsx
+++ layouts/Main/index.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import HeaderBar from "@components/HeaderBar";
+import { Wrapper } from "@pages/LogIn/styles";
 
 const Main = () => {
   return (
     <>
+    <Wrapper>
       <HeaderBar />
       <div>메인페이지입니당.</div>
+      </Wrapper>
     </>
   );
 };
