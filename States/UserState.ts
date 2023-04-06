83e6dff67050c3d5194699d7b1487bfd02aa475e
ChuSeongJun <91831079+ChuSeongJun@users.noreply.github.com>
2023-04-06 10:51:13 +0900

    Feat 유저타입 설정

---
+++ States/UserState.ts
@@ -0,0 +1,32 @@
+import { atom } from "recoil";
+
+export interface IUser {
+  isLoggingIn: boolean;
+  name: string;
+  age: number;
+  gender: string;
+  email: string;
+  password: string;
+  checkPassword: string;
+  birth: string;
+  jwt: string;
+  error: any;
+}
+
+const initialState: IUser = {
+  isLoggingIn: false,
+  name: "",
+  age: 0,
+  gender: "",
+  email: "",
+  password: "",
+  checkPassword: "",
+  birth: "",
+  jwt: "",
+  error: "",
+};
+
+export const UserState = atom({
+  key: "UserState",
+  default: initialState,
+});
