205d5b104978ffe75d5988cd07e6ba977d2cf1a9
ua1it <hoo9909@naver.com>
2023-04-11 20:31:25 +0900

    [ADD]설문 제작 페이지 컴포넌트 커스텀

--- pages/Survey/CreateSurvey/styles.tsx
+++ pages/Survey/CreateSurvey/styles.tsx
@@ -1,45 +1,65 @@
 import styled from "@emotion/styled";
 
 // dnd styles
-export const getQuestionTypeItemStyle = (isDragging: boolean, draggableStyle: any): React.CSSProperties => ({
-    userSelect: "none",
-    padding: 16,
-    margin: `0 0 8px 0`,
-    background: isDragging ? "lightgreen" : "grey",
-    ...draggableStyle
+export const getQuestionTypeItemStyle = (
+  isDragging: boolean,
+  draggableStyle: any
+): React.CSSProperties => ({
+  userSelect: "none",
+  padding: 16,
+  borderRadius: 10,
+  margin: `0 0 8px 0`,
+  boxShadow: `0px 2px 4px rgba(0,0,0,0.3)`,
+  background: isDragging ? "white" : "white",
+  ...draggableStyle,
 });
 
-export const getQuestionTypeListStyle = (isDraggingOver: boolean): React.CSSProperties => ({
-    background: isDraggingOver ? "lightblue" : "lightgrey",
-    padding: 8,
-    width: 250
+export const getQuestionTypeListStyle = (
+  isDraggingOver: boolean
+): React.CSSProperties => ({
+  borderRadius: 10,
+  background: isDraggingOver ? "white" : "white",
+  padding: 8,
+  width: 250,
 });
 
-export const getQuestionsItemStyle = (isDragging: boolean, draggableStyle: any): React.CSSProperties => ({
-    userSelect: 'none',
-    padding: 20,
-    margin: `0 0 10px 0`,
-    background: isDragging ? "green" : "white",
+export const getQuestionsItemStyle = (
+  isDragging: boolean,
+  draggableStyle: any
+): React.CSSProperties => ({
+  borderRadius: 10,
+  userSelect: "none",
+  padding: 20,
+  margin: `0 0 10px 0`,
+  boxShadow: `0px 2px 4px rgba(0,0,0,0.3)`,
+  background: isDragging ? "white" : "white",
+  ...draggableStyle,
+});
 
-    ...draggableStyle
-})
-
-export const getQuestionsListStyle = (isDraggingOver: boolean): React.CSSProperties => ({
-    background: isDraggingOver ? 'skyblue' : 'darkgrey',
-    padding: 90,
-    width: 450,
-})
-
+export const getQuestionsListStyle = (
+  isDraggingOver: boolean
+): React.CSSProperties => ({
+  borderRadius: 10,
+  border: "1px solid",
+  borderColor: "D3D3D3",
+  background: isDraggingOver ? "white" : "white",
+  padding: 90,
+  width: 650,
+});
 
 // component style
 export const CreateSurveyDiv = styled.div`
-    display: flex;
+  display: flex;
 `;
 
 export const QuestionTypeListDiv = styled.div`
-    flex: 1;
+  position: absolute;
+  top: 20%;
+  left: 20%;
 `;
 
 export const QuestionsListDiv = styled.div`
-    flex: 1;
+  position: absolute;
+  top: 20%;
+  right: 30%;
 `;
