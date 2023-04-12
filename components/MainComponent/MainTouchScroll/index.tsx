bda3ec94a3b78f8590845db0b989f0eddcfeba7c
jomo34 <trillion32@naver.com>
2023-04-13 00:01:56 +0900

    메인페이지 초기화면 구현

--- components/MainComponent/MainTouchScroll/index.tsx
+++ components/MainComponent/MainTouchScroll/index.tsx
@@ -2,9 +2,9 @@ import React from "react";
 import { Image } from "./styles";
 //https://www.npmjs.com/package/react-fast-marquee
 import Marquee from "react-fast-marquee";
-import test1 from "../../image/ScreenTest1.png";
-import test2 from "../../image/ScreenTest2.png";
-import test3 from "../../image/ScreenTest3.png";
+import test1 from "../../../image/ScreenTest1.png";
+import test2 from "../../../image/ScreenTest2.png";
+import test3 from "../../../image/ScreenTest3.png";
 
 const MainTouchScroll = () => {
   return (
