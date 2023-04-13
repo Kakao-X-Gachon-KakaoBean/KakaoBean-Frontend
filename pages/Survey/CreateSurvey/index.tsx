8e4619b50eeb003f2f41fd3230d7331b7067b4bf
ChuSeongJun <91831079+ChuSeongJun@users.noreply.github.com>
2023-04-14 08:40:21 +0900

    Feat: 챗봇 영역 설정

--- pages/Survey/CreateSurvey/index.tsx
+++ pages/Survey/CreateSurvey/index.tsx
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
 import CreateSurveyDnd from "@components/CreateSurveyDnd";
 
-import { CreateSurveyDiv } from "@pages/Survey/CreateSurvey/styles";
+import { ChatBtn, CreateSurveyDiv } from "@pages/Survey/CreateSurvey/styles";
+import ChatBot from "@components/ChatBot";
+import Menu from "@components/Menu";
 
 const CreateSurvey = (): JSX.Element => {
+  const [chatBotModal, setChatBotModal] = useState(false);
+
+  const onCloseChatBotModal = useCallback(() => {
+    setChatBotModal((prev) => !prev);
+  }, []);
   return (
     <CreateSurveyDiv>
       <CreateSurveyDnd />
+      <ChatBtn type="button" onClick={onCloseChatBotModal}>
+        챗봇 열어라
+      </ChatBtn>
+      {chatBotModal && (
+        <Menu show={chatBotModal} onCloseModal={onCloseChatBotModal}>
+          <ChatBot onCloseCheckChatBotModal={onCloseChatBotModal} />
+        </Menu>
+      )}
     </CreateSurveyDiv>
   );
 };
--- pages/Survey/CreateSurvey/styles.tsx
+++ pages/Survey/CreateSurvey/styles.tsx
@@ -3,8 +3,17 @@ import styled from "@emotion/styled";
 // component style
 export const CreateSurveyDiv = styled.div`
   display: grid;
-  grid-template-columns: 0.3fr 0.6fr;
+  grid-template-columns: 0.3fr 0.6fr 0.3fr;
 
   min-height: 100vh;
   margin-top: -2rem;
+`;
+
+export const ChatBtn = styled.button`
+  width: 5rem;
+  height: 5rem;
+  border-radius: 50%;
+  position: fixed;
+  bottom: 0;
+  right: 0;
 `;
