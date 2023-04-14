5f28ab1b75337ec5fdfa92b0594a672b8cdb63ff
ChuSeongJun <91831079+ChuSeongJun@users.noreply.github.com>
2023-04-14 21:17:13 +0900

    Feat: show 상태따른 return 값 삭제

--- components/Menu/index.tsx
+++ components/Menu/index.tsx
@@ -3,8 +3,6 @@ import { CreateModal } from "@components/Menu/styles";
 import { Props } from "@components/Menu/type";
 
 const Menu: FC<Props> = ({ children, show, onCloseModal, closeButton }) => {
-  if (!show) return null;
-
   return (
     <div>
       <CreateModal onClick={onCloseModal}>
