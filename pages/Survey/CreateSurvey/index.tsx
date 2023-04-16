5491e89d73d28851182d8cb5aaa8f591fd563372
ChuSeongJun <91831079+ChuSeongJun@users.noreply.github.com>
2023-04-16 23:15:28 +0900

    Chore: 챗봇 영역 수정

--- pages/Survey/CreateSurvey/index.tsx
+++ pages/Survey/CreateSurvey/index.tsx
@@ -13,20 +13,27 @@ const CreateSurvey = (): JSX.Element => {
   const [chatBotModal, setChatBotModal] = useState(false);
 
   const onCloseChatBotModal = useCallback(() => {
-    setChatBotModal((prev: any) => !prev);
-  }, [chatBotModal]);
+    setChatBotModal((prev: boolean) => !prev);
+  }, []);
 
-  console.log(chatBotModal);
+  const stopPropagation = useCallback(
+    (e: React.SyntheticEvent<EventTarget>) => {
+      e.stopPropagation();
+    },
+    []
+  );
 
   return (
     <CreateSurveyDiv>
       <CreateSurveyDnd />
       <Menu show={chatBotModal} onCloseModal={onCloseChatBotModal}>
-        {chatBotModal ? (
-          <Container>챗봇 영역</Container>
-        ) : (
-          <FalseContainer>챗봇 영역</FalseContainer>
-        )}
+        <div>
+          {chatBotModal ? (
+            <Container>챗봇 영역</Container>
+          ) : (
+            <FalseContainer>챗봇 영역</FalseContainer>
+          )}
+        </div>
       </Menu>
       <ChatBtn type="button" onClick={onCloseChatBotModal}>
         챗봇 열어라
