8172defe4268afaab3c635ec4d7a5722a44fc939
ChuSeongJun <91831079+ChuSeongJun@users.noreply.github.com>
2023-04-22 18:53:29 +0900

    Chore: 필요없는 파일 삭제

--- components/EmailModal/type.ts
+++
@@ -1,9 +0,0 @@
-import { ChangeEvent } from "react";
-
-export interface EmailModal {
-  email: string;
-  onChangeEmail: (e: ChangeEvent<HTMLInputElement>) => void;
-  onCloseCheckEmailModal: () => void;
-  authKey: string;
-  onChangeAuthKey: (e: ChangeEvent<HTMLInputElement>) => void;
-}
