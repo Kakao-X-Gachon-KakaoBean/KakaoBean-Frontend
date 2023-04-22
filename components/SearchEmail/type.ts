d22ea06590eb335334f38cd7d0213289304dfa98
ChuSeongJun <91831079+ChuSeongJun@users.noreply.github.com>
2023-04-22 18:54:03 +0900

    Feat: 이메일 찾기 구현

---
+++ components/SearchEmail/type.ts
@@ -0,0 +1,13 @@
+import { ChangeEvent } from "react";
+
+export interface EmailModal {
+  name: string;
+  onChangeName: (e: ChangeEvent<HTMLInputElement>) => void;
+  onCloseEmailModal: () => void;
+  birth: string;
+  onChangeBirth: (e: ChangeEvent<HTMLInputElement>) => void;
+}
+export interface Search {
+  name: string;
+  birth: string;
+}
