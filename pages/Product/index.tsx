1e6c4861a16b962e98998f505935252f8485c174
jomo34 <trillion32@naver.com>
2023-04-24 00:52:05 +0900

    로직 구현 중,,,

--- pages/Product/index.tsx
+++ pages/Product/index.tsx
@@ -11,7 +11,7 @@ import ReactFlow, {
 } from "react-flow-renderer";
 import { Logic } from "@pages/Product/type";
 import { RightSide, SideBar, Wrapper } from "@pages/Product/styles";
-import { Select, Space } from "antd";
+import { Select } from "antd";
 
 import { useRecoilState } from "recoil";
 import { countState } from "../../States/SurveyState";
@@ -30,7 +30,6 @@ const initialEdges = [{ id: "e1-2", source: "1", target: "2" }];
 const defaultViewport = { x: 400, y: 0, zoom: 1.5 };
 let id_num = 1;
 let QuestionList: any[] = [];
-let multiPossible = true;
 
 export default function Product() {
   const [input, setInput] = useState("0");
@@ -49,6 +48,7 @@ export default function Product() {
   ]);
 
   const [count, setCount] = useState<number[]>([0]);
+  const [isMultiCondition, setIsMultiCondition] = useState<number[]>([0]);
 
   const addLogic = () => {
     const updatedLogics = [...logics];
@@ -105,6 +105,8 @@ export default function Product() {
     setCount(updatedCounts);
   };
 
+  const addCondition = () => {};
+
   const onNodeClick = useCallback(
     (event) => {
       const targetId = Object.values(event.currentTarget.dataset.id);
@@ -121,7 +123,7 @@ export default function Product() {
     let yaxis = 0;
 
     // 여기서 i < ? 숫자 바꾸면 그 갯수만큼 생성
-    for (i; i < 3; i++) {
+    for (i; i < 5; i++) {
       let newNode, newEdge;
       if (i == 0) {
         newNode = {
@@ -155,6 +157,7 @@ export default function Product() {
       };
 
       setCount((prevCount) => [...prevCount, 0]);
+      setIsMultiCondition((prevVal) => [...prevVal, 0]);
       setLogics((prevLogic) => [...prevLogic, newLogic]);
       newNodeTuple.push(newNode);
       newEdgeTuple.push(newEdge);
@@ -190,7 +193,8 @@ export default function Product() {
     const targetLogic = updatedLogics[selNodeNumber].logics[i];
     const isInitialValue = targetLogic.conditionOfQuestionAnswers[0] === "0";
 
-    if (multiPossible) {
+    // 조건 여러개일때
+    if (isMultiCondition[selNodeNumber]) {
       if (isInitialValue) {
         targetLogic.conditionOfQuestionAnswers = [value];
       } else {
@@ -199,28 +203,9 @@ export default function Product() {
         }
       }
     }
-    // 중복 허용되지 않을 때 처리
+    // 조건 하나일때
     else {
-      const updatedLogics = logics.map((logic, index) => {
-        if (index !== Number(selNode)) {
-          return logic;
-        } else {
-          const updatedLogic = {
-            id: logic.id,
-            logics: logic.logics.map((condition, conditionIndex) => {
-              if (conditionIndex !== i) {
-                return condition;
-              } else {
-                return {
-                  conditionOfQuestionAnswers: [value],
-                  nextQuestionNumber: condition.nextQuestionNumber,
-                };
-              }
-            }),
-          };
-          return updatedLogic;
-        }
-      });
+      targetLogic.conditionOfQuestionAnswers = [value];
     }
 
     setLogics(updatedLogics);
@@ -229,22 +214,71 @@ export default function Product() {
   const NextQuestionChange = (i: number, value: string) => {
     const updatedLogics = [...logics];
     let updatedEdges = [...edges];
+    let updatedNodes = [...nodes];
     const newEdge: Edge = {
       id: "e" + selNode + "-" + value,
       source: String(selNode),
       target: String(value),
+      animated: true,
     };
 
     updatedLogics[Number(selNode)].logics[i].nextQuestionNumber = value;
     setLogics(updatedLogics);
 
     if (i == 0) {
-      updatedEdges = edges.filter((edge) => edge.source !== selNode);
+      //updatedEdges = edges.filter((edge) => edge.source !== selNode);
+
+      updatedNodes.forEach((node) => {
+        if (
+          node.id === String(selNode) ||
+          node.id === String(value) ||
+          node.id === "0" ||
+          node.id > value
+        ) {
+        } else {
+          node.position.x = node.position.x + 100;
+        }
+      });
     }
     updatedEdges.push(newEdge);
     setEdges(updatedEdges);
+    setNodes(updatedNodes);
 
-    console.log(edges);
+    //console.log(edges);
+  };
+
+  const NoLogicChangeNext = (value: string) => {
+    let updatedEdges = [...edges];
+    let updatedNodes = [...nodes];
+    const newEdge: Edge = {
+      id: "e" + selNode + "-" + value,
+      source: String(selNode),
+      target: String(value),
+      animated: true,
+    };
+
+    updatedEdges = edges.filter(
+      (edge) =>
+        edge.source === selNode && edge.target === String(Number(selNode) + 1)
+    );
+
+    let flag = updatedNodes.find((node) => node.id === selNode);
+
+    if (selNode && flag) {
+      const selNodeY = flag.position.y;
+      updatedNodes.forEach((node) => {
+        if (node.id < selNode && node.id > value) {
+          node.position.x = node.position.x + 100;
+          node.position.y = selNodeY;
+        }
+        if (node.id >= value) {
+          node.position.x = node.position.x + 50;
+        }
+      });
+    }
+    updatedEdges.push(newEdge);
+    setEdges(updatedEdges);
+    setNodes(updatedNodes);
   };
 
   return (
@@ -255,6 +289,15 @@ export default function Product() {
         ) : (
           <div>
             <div>질문 {selNode}</div>
+            <div>
+              이동하기
+              <Select
+                defaultValue="다음질문"
+                style={{ width: 120 }}
+                onChange={NoLogicChangeNext}
+                options={QuestionList}
+              />
+            </div>
             <Button
               onClick={(e) => {
                 addLogic();
@@ -267,50 +310,30 @@ export default function Product() {
                 {logics[Number(selNode)].logics.map((logic, i) => (
                   <div key={i}>
                     <div>로직 {i + 1}</div>
-                    {multiPossible ? (
-                      <div>
-                        조건 :
-                        <Select
-                          defaultValue={logic.conditionOfQuestionAnswers[0]}
-                          style={{ width: 120 }}
-                          onChange={(e) => ConditionChange(i, e)}
-                          options={[
-                            { value: "1", label: "1" },
-                            { value: "2", label: "2" },
-                            { value: "3", label: "3" },
-                            { value: "4", label: "4" },
-                          ]}
-                        />
-                        <Select
-                          defaultValue={logic.conditionOfQuestionAnswers[1]}
-                          style={{ width: 120 }}
-                          onChange={(e) => ConditionChange(i, e)}
-                          options={[
-                            { value: "1", label: "1" },
-                            { value: "2", label: "2" },
-                            { value: "3", label: "3" },
-                            { value: "4", label: "4" },
-                          ]}
-                        />
-                      </div>
-                    ) : (
-                      <div>
-                        조건 :
-                        <Select
-                          defaultValue={logic.conditionOfQuestionAnswers[0]}
-                          style={{ width: 120 }}
-                          onChange={(e) => ConditionChange(i, e)}
-                          options={[
-                            { value: "1", label: "1" },
-                            { value: "2", label: "2" },
-                            { value: "3", label: "3" },
-                            { value: "4", label: "4" },
-                          ]}
-                        />
-                      </div>
-                    )}
                     <div>
-                      다음질문 :
+                      조건 :
+                      <Select
+                        defaultValue={logic.conditionOfQuestionAnswers[0]}
+                        style={{ width: 120 }}
+                        onChange={(e) => ConditionChange(i, e)}
+                        options={[
+                          { value: "1", label: "1" },
+                          { value: "2", label: "2" },
+                          { value: "3", label: "3" },
+                          { value: "4", label: "4" },
+                        ]}
+                      />
+                      와 같다면
+                      <Button
+                        onClick={(e) => {
+                          addCondition();
+                        }}
+                      >
+                        조건 추가 하기
+                      </Button>
+                    </div>
+                    <div>
+                      이동 :
                       <Select
                         defaultValue={logic.nextQuestionNumber}
                         style={{ width: 120 }}
@@ -318,13 +341,12 @@ export default function Product() {
                         options={QuestionList}
                       />
                     </div>
-                    {/*<div>{JSON.stringify(logic)}</div>*/}
                   </div>
                 ))}
                 <div>{JSON.stringify(logics[Number(selNode)])}</div>
               </>
             ) : (
-              <div>로직이 없습니다.</div>
+              <div></div>
             )}
           </div>
         )}
