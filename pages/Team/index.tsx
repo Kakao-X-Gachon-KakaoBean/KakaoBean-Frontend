2cbc0e1be7e5d9a183c2e2c6f291bcc64edf7309
ChuSeongJun <91831079+ChuSeongJun@users.noreply.github.com>
2023-04-25 21:30:08 +0900

    Feat: 설문 생성 기본 틀 생성완료 버튼 구현 완료

--- pages/Team/index.tsx
+++ pages/Team/index.tsx
@@ -1,10 +1,56 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import HeaderBar from "@components/HeaderBar";
+import { Carousel, Button } from "antd";
+import { CarouselRef } from "antd/es/carousel";
+
+const contentStyle: React.CSSProperties = {
+  height: "480px",
+  color: "#fff",
+  lineHeight: "480px",
+  textAlign: "center",
+  background: "#364d79",
+};
 
 const Team = () => {
+  const carouselRef = useRef<CarouselRef>(null);
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  useEffect(() => {
+    carouselRef.current && carouselRef.current.goTo(currentSlide);
+  }, [currentSlide]);
+
+  const handlePrevClick = () => {
+    carouselRef.current?.prev();
+    setCurrentSlide(currentSlide - 1);
+  };
+
+  const handleNextClick = () => {
+    carouselRef.current?.next();
+    setCurrentSlide(currentSlide + 1);
+  };
+
   return (
     <div>
-      <div>팀 페이지</div>
+      <Carousel dotPosition={"right"} ref={carouselRef}>
+        <div>
+          <h3 style={contentStyle}>1</h3>
+        </div>
+        <div>
+          <h3 style={contentStyle}>2</h3>
+        </div>
+        <div>
+          <h3 style={contentStyle}>3</h3>
+        </div>
+        <div>
+          <h3 style={contentStyle}>4</h3>
+        </div>
+      </Carousel>
+      <Button disabled={currentSlide === 0} onClick={handlePrevClick}>
+        이전
+      </Button>
+      <Button disabled={currentSlide === 3} onClick={handleNextClick}>
+        다음
+      </Button>
     </div>
   );
 };
