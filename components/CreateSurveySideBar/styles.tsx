92c0b3a64aa9bda3fc791f4012857d47da3df53c
ua1it <hoo9909@naver.com>
2023-04-26 16:31:54 +0900

    [ADD]설문생성 사이드 바 antd menu 적용

--- components/CreateSurveySideBar/styles.tsx
+++ components/CreateSurveySideBar/styles.tsx
@@ -1,6 +1,15 @@
 import styled from "@emotion/styled";
 
 export const OptionDiv = styled.div`
+  display: flex;
+  position: fixed;
+  width: 20rem;
+  height: auto;
+  right: 4rem;
   padding-top: 10rem;
-  justify-content: center;
+`;
+
+export const MenuDiv = styled.div`
+  display: flex;
+  flex: 1;
 `;
