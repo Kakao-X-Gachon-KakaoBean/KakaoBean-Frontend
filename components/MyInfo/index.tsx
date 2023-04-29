64a6d356a6603a182b5f64ec2ae40d85a4ffe121
ChuSeongJun <91831079+ChuSeongJun@users.noreply.github.com>
2023-04-30 00:16:15 +0900

    Feat: 마이페이지 정보 불러오기

--- components/MyInfo/index.tsx
+++ components/MyInfo/index.tsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { useQuery } from "react-query";
+import fetcher from "@utils/fetcher";
 
 const MyInfo = () => {
-  return <div>내 정보</div>;
+  const { isLoading, isSuccess, status, isError, data, error } = useQuery(
+    ["user"],
+    () => fetcher({ queryKey: "http://localhost:8080/member/member-info" })
+  );
+  console.log(data);
+  return (
+    <>
+      <div>내 정보</div>
+      <div>이름: {data}</div>
+      <div>나이: {data}</div>
+      <div>성별: {data}</div>
+      <div>이메일: {data}</div>
+      <div>생일: {data}</div>
+    </>
+  );
 };
 
 export default MyInfo;
