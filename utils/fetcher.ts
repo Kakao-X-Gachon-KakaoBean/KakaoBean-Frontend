71446d0bfad492954e68b4e8b871193741df9a60
ChuSeongJun <91831079+ChuSeongJun@users.noreply.github.com>
2023-05-05 08:34:27 +0900

    Feat: 헤더 정보 추가

--- utils/fetcher.ts
+++ utils/fetcher.ts
@@ -1,13 +1,11 @@
 import axios from "axios";
 
-const headers = {
-  "X-Requested-With": "XMLHttpRequest",
-};
 //get
 const fetcher = async ({ queryKey }: { queryKey: string }) => {
   const response = await axios.get(queryKey, {
     withCredentials: true,
     headers: {
+      "X-Requested-With": "XMLHttpRequest",
       Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
     },
   });
