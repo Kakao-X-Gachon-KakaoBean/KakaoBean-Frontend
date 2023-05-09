ae14ddc850990120aab3cde85914948be500b58f
ChuSeongJun <91831079+ChuSeongJun@users.noreply.github.com>
2023-05-09 23:45:20 +0900

    Feat: 참여 설문 조회 페이지 생성

---
+++ components/SurveyList/index.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import {
+  SurveyBox,
+  SurveyContainer,
+  SurveyHeader,
+  SurveyResult,
+  SurveyTitle,
+  SurveyInfo,
+} from "@components/MySurvey/styles";
+
+const SurveyList = () => {
+  return (
+    <>
+      <SurveyHeader>참여 설문 조회</SurveyHeader>
+      <SurveyContainer>
+        {/*{data &&*/}
+        {/*  [...Array(data?.length)].map((e, index) => {*/}
+        {/*    const SurveyId = SurveyList[index].surveyid;*/}
+        {/*    return (*/}
+        <SurveyBox>
+          <SurveyInfo>
+            <SurveyTitle>제목</SurveyTitle>
+            <SurveyResult>응답 개수</SurveyResult>
+          </SurveyInfo>
+        </SurveyBox>
+        {/*  );*/}
+        {/*})}*/}
+      </SurveyContainer>
+    </>
+  );
+};
+
+export default SurveyList;
