d5bdb1073884f91677bc6ffda836ae4fd5bf8f59
ChuSeongJun <91831079+ChuSeongJun@users.noreply.github.com>
2023-05-20 21:50:28 +0900

    Feat: 참여한 설문 조회 api 추가

--- components/SurveyList/index.tsx
+++ components/SurveyList/index.tsx
@@ -9,17 +9,30 @@ import {
 } from "@components/MySurvey/styles";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrashCan } from "@fortawesome/free-regular-svg-icons";
+import { useQuery } from "react-query";
+import fetcher from "@utils/fetcher";
 
 const SurveyList = () => {
+  // const {
+  //   isLoading,
+  //   isSuccess,
+  //   status,
+  //   isError,
+  //   data: SurveyList,
+  //   error,
+  // } = useQuery(["SurveyList"], () =>
+  //   fetcher({ queryKey: "http://localhost:8080/surveys/" })
+  // );
+
   return (
     <>
       <SurveyHeader>참여 설문 조회</SurveyHeader>
 
       <SurveyContainer>
-        {/*{data &&*/}
-        {/*  [...Array(data?.length)].map((e, index) => {*/}
-        {/*    const SurveyId = SurveyList[index].surveyid;*/}
-        {/*    return (*/}
+        {/*{SurveyList &&*/}
+        {/*[...Array(MySurvey?.myOwnSurveys.length)].map((e, index) => {*/}
+        {/*const SurveyId = MySurvey?.myOwnSurveys[index].surveyid;*/}
+        {/*return (*/}
         <SurveyBox>
           <SurveyInfo>
             <SurveyTitle>제목</SurveyTitle>
