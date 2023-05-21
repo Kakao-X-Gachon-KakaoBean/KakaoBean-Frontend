bd19e69606b96a3542cc3f19f5f4933a3306c202
BetterTomorrowThanToday <wsx0239@gmail.com>
2023-05-22 08:18:13 +0900

    feat) API out

--- components/SurveyResponseTemplates/SurveyData/surveyOut.tsx
+++ components/SurveyResponseTemplates/SurveyData/surveyOut.tsx
@@ -1,1 +1,19 @@
-export {};
+import { useMutation } from "react-query";
+import { responseDataList } from "@pages/Team/type";
+
+export const postData = async (url: string, data: responseDataList) => {
+  const response = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+
+  if (!response.ok) {
+    throw new Error("API 요청이 실패했습니다.");
+  }
+
+  return response.json();
+};
+// export {};
