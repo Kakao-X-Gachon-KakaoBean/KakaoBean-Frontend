ce6a5ff2cbc2433e5b703be4d91aeb95cff894c3
jomo34 <trillion32@naver.com>
2023-05-26 10:20:05 +0900

    비밀번호 변경 구현. (백에 따라 url은 수정해야할수도)

--- components/PasswordModal/index.tsx
+++ components/PasswordModal/index.tsx
@@ -1,20 +1,34 @@
-import React, { FC, FormEvent, useCallback, useState } from "react";
+import React, {
+  ChangeEvent,
+  FC,
+  FormEvent,
+  useCallback,
+  useState,
+} from "react";
 
-import {
-  Button,
-  Div,
-  Form,
-  Header,
-  InputInfo,
-} from "@components/SearchEmail/styles";
-
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import axios, { AxiosError } from "axios";
 
-import { Wrapper } from "@components/PasswordModal/styles";
-import { InputKey } from "@components/SearchEmail/styles";
+import {
+  Wrapper,
+  CheckBtn,
+  Correct,
+  Form,
+  EmailLabel,
+  Error,
+  Input,
+  Div,
+  Header,
+  Button,
+  InputKey,
+  Label,
+  InputKeyWithText,
+} from "@components/PasswordModal/styles";
 import { PaswwordModal } from "@components/PasswordModal/type";
 import { useMutation } from "react-query";
 import { Search } from "@components/SearchEmail/type";
+import useInput from "../../hooks/useInput";
 
 const SearchPassword: FC<PaswwordModal> = ({
   name,
@@ -23,16 +37,77 @@ const SearchPassword: FC<PaswwordModal> = ({
   birth,
   onChangeBirth,
 }) => {
+  const [email, onChangeEmail, setEmail] = useInput("");
+  const [password, , setPassword] = useInput("");
+  const [checkPassword, , setCheckPassword] = useInput("");
+  const [emailAuthKey, onChangeEmailAuthKey, seyAuthKey] = useInput("");
+  const [failUseEmail, setFailUseEmail] = useState(false);
+  const [mismatchError, setMismatchError] = useState(false);
+
+  //입력한 이메일로 인증번호 보내기
+  const onSubmitEmail = useCallback(
+    (e?: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+      e?.preventDefault();
+      const message = (message: string) => (
+        <div style={{ fontSize: "1rem" }}>{message}</div>
+      );
+
+      if (!email || !email.trim()) return;
+
+      axios
+        .post(
+          "http://localhost:8080/emails",
+          { email },
+          { withCredentials: true }
+        )
+        .then((response) => {
+          setFailUseEmail(true);
+          toast(message("메일로 인증번호가 발송되었습니다."), {
+            type: "success",
+          });
+        })
+        .catch((error) => {
+          toast(message("메일 주소를 확인해주세요."), { type: "error" });
+          setFailUseEmail(false);
+          console.log(error.response);
+        });
+    },
+    [email]
+  );
+
+  const onChangePassword = useCallback(
+    (e: ChangeEvent<HTMLInputElement>) => {
+      setPassword(e.target.value);
+      setMismatchError(e.target.value === checkPassword);
+    },
+    [password, setPassword]
+  );
+
+  const onChangeCheckPassword = useCallback(
+    (e: ChangeEvent<HTMLInputElement>) => {
+      setCheckPassword(e.target.value);
+      setMismatchError(e.target.value === password);
+    },
+    [password, setCheckPassword]
+  );
+
   const stopPropagation = useCallback(
     (e: React.SyntheticEvent<EventTarget>) => {
       e.stopPropagation();
     },
     []
   );
+
+  //url 수정 필요할수도
   const mutation = useMutation<
     Search,
     AxiosError,
-    { name: string; birth: string }
+    {
+      email: string;
+      emailAuthKey: string;
+      password: string;
+      checkPassword: string;
+    }
   >(
     "searchEmail",
     (data) =>
@@ -54,29 +129,96 @@ const SearchPassword: FC<PaswwordModal> = ({
       },
     }
   );
+
   const onSubmit = useCallback(
     (e) => {
       e.preventDefault();
-      mutation.mutate({ name, birth });
+      mutation.mutate({ email, emailAuthKey, password, checkPassword });
     },
-    [name, birth, mutation]
+    [email, emailAuthKey, password, checkPassword, mutation]
   );
 
   return (
     <Wrapper onClick={stopPropagation}>
       <Form>
         <Div>
-          <Header>비밀번호 찾기?</Header>
+          <Header>비밀번호 변경</Header>
         </Div>
         <button onClick={onClosePasswordModal}>X</button>
       </Form>
 
+      <EmailLabel>
+        <Input
+          type="email"
+          id="email"
+          name="email"
+          value={email}
+          onChange={onChangeEmail}
+          placeholder="이메일"
+        />
+        <CheckBtn
+          type="button"
+          onClick={(e) => {
+            onSubmitEmail(e);
+          }}
+        >
+          이메일 인증
+        </CheckBtn>
+      </EmailLabel>
+
+      <Label>
+        <Input
+          type="text"
+          id="authKey"
+          name="authKey"
+          value={emailAuthKey}
+          onChange={onChangeEmailAuthKey}
+          placeholder="인증번호 입력"
+        />
+      </Label>
+      <Label>
+        <Input
+          type="password"
+          id="password"
+          name="password"
+          value={password}
+          onChange={onChangePassword}
+          placeholder="비밀번호"
+        />
+      </Label>
+      <Label>
+        <Input
+          type="password"
+          id="passwordCheck"
+          name="passwordCheck"
+          value={checkPassword}
+          onChange={onChangeCheckPassword}
+          placeholder="비밀번호 확인"
+        />
+        {!mismatchError && checkPassword.length >= 1 && (
+          <Error>비밀번호가 일치하지 않습니다!</Error>
+        )}
+        {mismatchError && checkPassword.length >= 1 && (
+          <Correct>비밀번호가 일치합니다!</Correct>
+        )}
+      </Label>
+
       {/*{failUseEmail && !proveEmail && (*/}
-      <InputKey>
-        <Form onSubmit={onSubmit}>
-          <Button type="submit">인증 확인</Button>
-        </Form>
-      </InputKey>
+      {(!mismatchError && checkPassword.length >= 1) ||
+      (mismatchError && checkPassword.length >= 1) ? (
+        <InputKeyWithText>
+          <Form onSubmit={onSubmit}>
+            <Button type="submit">비밀번호 변경</Button>
+          </Form>
+        </InputKeyWithText>
+      ) : (
+        <InputKey>
+          <Form onSubmit={onSubmit}>
+            <Button type="submit">비밀번호 변경</Button>
+          </Form>
+        </InputKey>
+      )}
+
       {/*)}*/}
     </Wrapper>
   );
--- components/PasswordModal/styles.tsx
+++ components/PasswordModal/styles.tsx
@@ -1,8 +1,9 @@
 import styled from "@emotion/styled";
+import { keyframes } from "@emotion/react";
 
 export const Wrapper = styled.div`
   background-color: white;
-  border-radius: 5px;
+  border-radius: 10px;
   box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.3);
   position: absolute;
   top: 50%;
@@ -12,12 +13,11 @@ export const Wrapper = styled.div`
 
   width: 25vw;
   min-width: 400px;
-  height: 30vh;
+  height: 55vh;
   min-height: 180px;
 
   display: flex;
   flex-direction: column;
-  justify-content: center;
   align-items: center;
 
   box-sizing: border-box;
@@ -59,7 +59,12 @@ export const Wrapper = styled.div`
 `;
 
 export const InputKey = styled.div`
-  margin-top: 1rem;
+  margin-top: 1.2rem;
+  display: flex;
+  justify-content: center;
+`;
+
+export const InputKeyWithText = styled.div`
   display: flex;
   justify-content: center;
 `;
@@ -88,19 +93,56 @@ export const EmailInput = styled.input`
   }
 `;
 
+const checkButtonAnimation = keyframes`
+    to{
+      background-color: #039ba1;
+      color: white;
+      border: none;
+    }
+`;
+
+export const CheckBtn = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 6rem;
+  height: 2rem;
+  background: #f2fcfc;
+  border: 1px solid #b6e4e5;
+  border-radius: 8px;
+  outline: none;
+  font-size: 0.8rem;
+  font-weight: 700;
+  color: #b6e4e5;
+  cursor: pointer;
+  margin-top: 0.9rem;
+  margin-bottom: 1.3rem;
+  &:hover {
+    animation: ${checkButtonAnimation} 0.2s ease-in-out forwards;
+  }
+`;
+
+export const EmailLabel = styled.div`
+  margin-bottom: 0.4rem;
+  display: flex;
+  width: 20rem;
+  height: 2rem;
+  justify-content: center;
+  align-items: center;
+  gap: 1rem;
+`;
+
 export const Form = styled.form`
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  margin: 0 auto;
 `;
 
 export const Div = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-  margin-bottom: 1rem;
 `;
 
 export const Header = styled.header`
@@ -108,26 +150,59 @@ export const Header = styled.header`
   font-weight: bold;
 `;
 
+export const Label = styled.div`
+  margin-bottom: 0.4rem;
+
+  & span {
+    text-align: left;
+    font-size: 15px;
+    cursor: pointer;
+    font-weight: 700;
+    width: 7rem;
+  }
+`;
+
+const buttonAnimation = keyframes`
+  from {
+    background-color: #f1f3f5;
+  }
+
+  to {
+    background-color: #039ba1;
+    color: white;
+    border: none;
+  }
+`;
+
 export const Button = styled.button`
+  color: #b8c0c5;
   display: flex;
   justify-content: center;
   align-items: center;
   width: 7rem;
   height: 2.3rem;
-  background-color: #8cafa3;
   box-shadow: 0 4px 4px 0 #00000040;
-  color: white;
   border-radius: 10px;
-  outline: none;
   font-size: 0.8rem;
   font-weight: 700;
   cursor: pointer;
   border: none;
-  margin-bottom: 0.3rem;
+  margin-top: 1.5rem;
   &:hover {
-    background-color: #969696;
-    border: none;
+    animation: ${buttonAnimation} 0.2s ease-in-out forwards;
   }
+`;
+
+export const Error = styled.div`
+  color: red;
+  font-weight: bold;
+  font-size: small;
+`;
+
+export const Correct = styled.div`
+  color: dodgerblue;
+  font-weight: bold;
+  font-size: small;
 `;
 
 export const Input = styled.input`
@@ -140,4 +215,5 @@ export const Input = styled.input`
   height: 2rem;
   outline: none;
   font-size: 1rem;
+  margin-bottom: 0.5rem;
 `;
