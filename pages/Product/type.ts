91d7976446eb8ecfef312b6fdb67bd830439beb1
ChuSeongJun <91831079+ChuSeongJun@users.noreply.github.com>
2023-06-01 09:57:46 +0900

    Feat: 설문 통계 데이터 타입 설정

---
+++ pages/Product/type.ts
@@ -0,0 +1,18 @@
+export interface SurveyDataType {
+  surveyId: number;
+  surveyTitle: string;
+  surveyDate: string;
+  numberOfResponse: number;
+  surveyGenderPercent: { name: string; value: number }[];
+  surveyAgePercent: { name: string; 인원수: number }[];
+  questionsResult: QuestionResult[];
+}
+
+interface QuestionResult {
+  type: "RANGE" | "MULTIPLE" | "ESSAY";
+  title: string;
+  explanation: string;
+  min?: number;
+  max?: number;
+  answers: { name: string; value: number }[] | string[];
+}
