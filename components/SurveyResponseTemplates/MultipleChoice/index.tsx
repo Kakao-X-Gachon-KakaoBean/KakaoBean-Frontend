46c93a39bc73a1b58d4df44814052becdda2c618
kimyunho <31121701+ua1it@users.noreply.github.com>
2023-06-01 16:30:44 +0900

    [ADD]객관식 설문 응답 수 지정

--- components/SurveyResponseTemplates/MultipleChoice/index.tsx
+++ components/SurveyResponseTemplates/MultipleChoice/index.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useRecoilState } from "recoil";
 import { forLogic, report, submitAll } from "@pages/Team";
-
+import { Button, Modal } from "antd";
 import {
   Answer,
   MultipleQuestion,
@@ -15,6 +15,7 @@ import { responseQuestionType } from "@pages/Team/type";
 import { SpaceBetween } from "@pages/Team/styles";
 import {
   Explanation,
+  PS,
   QuestionBox,
   Title,
 } from "@components/SurveyResponseTemplates/styles";
@@ -42,6 +43,7 @@ export const MultipleChoiceQuestions = (props: subProps) => {
   const [makeData, setMakeData] = useState<Answer[]>([]);
   const [reportData, setReportData] = useRecoilState(report);
   const [submitMultiple] = useRecoilState(submitAll);
+  const [isOverNumberOfAnswer, setIsOverNumberOfAnswer] = useState(false);
 
   //logic Mapping
   const [logic, setLogic] = useRecoilState(forLogic);
@@ -50,16 +52,23 @@ export const MultipleChoiceQuestions = (props: subProps) => {
     question.logics.map(() => ({ id: [0], next: "" }))
   );
 
+  //toast message
   //버튼 클릭에 따른 바뀐 선택지 state true로 적용 vice versa
   const handleCheckboxChange = (index: number) => {
     setCheckboxData((prevState) => {
       const newState = [...prevState];
       newState[index].checked = !newState[index].checked;
+
       //이에 따른 로직 업데이트
       if (newState[index].checked) {
-        setSelectedIdBox((prev) => {
-          return [...prev].concat(question.answers[index].answerId);
-        });
+        if (selectedIdBox.length > question.numberOfAnswerChoices) {
+          setIsOverNumberOfAnswer(true);
+          newState[index].checked = false;
+        } else {
+          setSelectedIdBox((prev) => {
+            return [...prev].concat(question.answers[index].answerId);
+          });
+        }
       } else {
         setSelectedIdBox((prev) => {
           return [...prev].filter(
@@ -165,8 +174,28 @@ export const MultipleChoiceQuestions = (props: subProps) => {
 
   return (
     <QuestionBox>
+      <Modal
+        title="질문의 응답 수가 정해져있습니다"
+        centered
+        open={isOverNumberOfAnswer}
+        onCancel={() => setIsOverNumberOfAnswer(false)}
+        footer={[
+          <Button type="primary" onClick={() => setIsOverNumberOfAnswer(false)}>
+            확인
+          </Button>,
+        ]}
+      >
+        <p>
+          이 문제는 {question.numberOfAnswerChoices}개의 답으로 응답해 주셔야
+          합니다.
+        </p>
+      </Modal>
       <Title>{question.title}</Title>
       <Explanation>{question.explanation}</Explanation>
+      <PS>
+        이 문제는 {question.numberOfAnswerChoices}개의 답으로 응답해 주셔야
+        합니다.
+      </PS>
       {question.answers.map((question, index) => (
         <MultipleQuestionDiv key={index}>
           <ChoiceBtn
