2b6899acb046513f5cf236f22f9458c99655b213
kimyunho <31121701+ua1it@users.noreply.github.com>
2023-06-01 21:29:19 +0900

    [ADD]설문 응답 완료 시 메시지 애니메이션 등록

--- components/SurveyResponseTemplates/SubmitionCompletePage/index.tsx
+++ components/SurveyResponseTemplates/SubmitionCompletePage/index.tsx
@@ -8,8 +8,18 @@ import {
 import { SpaceBetween } from "@pages/Team/styles";
 import { report } from "@pages/Team";
 import { useMutation } from "react-query";
+import Lottie from "lottie-react";
+import successAnimation from "../../../animation/7893-confetti-cannons.json";
+import failAnimation from "../../../animation/119777-fail.json";
 import axios, { AxiosError } from "axios";
 import { responseDataList } from "../../../pages/Team/type";
+import { Button } from "antd";
+import {
+  EndingMessageDiv,
+  LottieContainer,
+  MessageContainer,
+  Wrapper,
+} from "./styles";
 
 export const EndingPage = () => {
   const [reportData, setReportData] = useRecoilState(report);
@@ -44,12 +54,31 @@ export const EndingPage = () => {
   }, []);
 
   return (
-    <QuestionBox>
+    <Wrapper>
       {check ? (
-        <Title id={"canvas"}>응답 제출이 완료되었습니다!</Title>
+        <MessageContainer>
+          <LottieContainer>
+            <Lottie animationData={successAnimation} />
+          </LottieContainer>
+          <EndingMessageDiv>응답 제출이 완료되었습니다!</EndingMessageDiv>
+        </MessageContainer>
       ) : (
-        <Title>응답 제출에 실패하였습니다.</Title>
+        <MessageContainer>
+          <LottieContainer>
+            <Lottie animationData={failAnimation} />
+          </LottieContainer>
+          <EndingMessageDiv>
+            응답 제출에 실패하였습니다..{" "}
+            <span
+              onClick={() => window.location.reload()}
+              style={{ color: "blue", cursor: "pointer" }}
+            >
+              다시 시도
+            </span>
+            해주세요
+          </EndingMessageDiv>
+        </MessageContainer>
       )}
-    </QuestionBox>
+    </Wrapper>
   );
 };
