7bf884364a567dd44940beadcf0fc97be04fb09a
jomo34 <trillion32@naver.com>
2023-06-06 18:24:48 +0900

    프론트 엔드 커밋

--- cypress/e2e/2.main_page/main_page.cy.ts
+++ cypress/e2e/2.main_page/main_page.cy.ts
@@ -14,9 +14,12 @@ describe("메인 페이지", () => {
       cy.get(".css-1u7g3ak").should("be.visible");
 
       //여기서부턴 스크롤 내려야 보임
+      cy.scrollTo("center");
       cy.get(".css-1l5y9fy > div.aos-init > .css-rxxf9o").should("be.visible");
       cy.get(".css-1l5y9fy > div.aos-init > .css-end85d").should("be.visible");
       cy.get("video.aos-init").should("be.visible");
+
+      cy.scrollTo("bottom");
       cy.get(".css-1e91d6q > .css-rxxf9o").should("be.visible");
       cy.get(".css-1e91d6q > :nth-child(2)").should("be.visible");
       cy.get(".css-1e91d6q > :nth-child(3)").should("be.visible");
@@ -28,6 +31,7 @@ describe("메인 페이지", () => {
       cy.get(".css-lc7fha").should("be.visible");
     });
   });
+
   context("Get Start 버튼을 클릭했을 경우", () => {
     it("로그인 상태를 확인하고, 로그인 상태면 설문생성으로 이동해야한다.", () => {
       // localStorage를 확인해서 로그인 상태인지 확인
@@ -40,6 +44,23 @@ describe("메인 페이지", () => {
             .and("contain.text", "Get Start")
             .click({ force: true });
           cy.url().should("include", "/createsurvey");
+          cy.get(".css-64wk4k").click();
+        } else {
+          cy.log("no accessToken");
+        }
+      });
+    });
+    it("로그인 상태를 확인하고, 로그인 하지 않은 상태면 로그인 페이지로 이동해야한다.", () => {
+      // localStorage를 확인해서 로그인 상태인지 확인
+      cy.window().then((win) => {
+        const accessToken = win.localStorage.getItem("accessToken");
+        // mockAccessToken으로 테스트 가능
+        if (accessToken === undefined) {
+          cy.get(".ant-btn > a")
+            .should("exist")
+            .and("contain.text", "Get Start")
+            .click({ force: true });
+          cy.url().should("include", "/login");
           cy.get(".css-64wk4k").click();
         } else {
           cy.log("no accessToken");
